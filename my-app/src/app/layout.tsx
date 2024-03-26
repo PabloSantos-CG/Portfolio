c1918205fa8053f2dcc1171f46030add7a6e5d24
PabloSantos-CG <comercial-pablo@outlook.com>
2024-03-26 20:06:30 -0300

    refactor:  add metadata

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -4,11 +4,13 @@ import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PS-Software Developer",
+  creator: "Pablo Santos Cezar Gomes",
+  title: "PS Software Developer",
+  description:
+    "Programador, capacitado para atender a suas demandas e executar seus projetos. Especializado em tecnologias não só do Front-End, com React.Js e Next.js, como também do Back-End, utilizando Node.Js.",
 };
 
 export default function RootLayout({
